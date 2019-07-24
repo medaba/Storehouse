0358863ad2701ab86e7adcce621948397b1f4425
medaba@protonmail.com <medaba@protonmail.com>
2019-07-24 19:21:40 +0700

    random password

---
+++ random_password.py
@@ -0,0 +1,19 @@
+# Рандомный пароль вида: skREpBgKVb9IM3KomGrxqccKRhWHgGywdOA99aEAZh
+
+from string import ascii_letters, digits
+import random
+
+
+def generate_password(size=48):
+    """ Создание рандомного пароля определенной длины из букв и цифр.
+    """
+    
+    chars = ascii_letters + digits    # ascii_letters: a-z + A-Z, digits: 0-9.
+
+    password = ''.join(random.choice(chars) for x in range(size))
+    
+    return password
+
+
+
+print(generate_password(42))
