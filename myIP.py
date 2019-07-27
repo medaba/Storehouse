3f8efacbc20c9664a5a139f7b9839f92015f411b
medaba@protonmail.com <medaba@protonmail.com>
2019-07-28 02:43:37 +0700

    + whois

--- myIP.py
+++ myIP.py
@@ -3,6 +3,8 @@
 # Делает запрос к "api.ipify.org" и возвращает ваш текущий IP.
 
 import requests
+import whois
+from pprint import pprint
 
 def get_ip():
     r = requests.get('https://api.ipify.org?format=json')
@@ -13,8 +15,17 @@ def get_ip():
     else:
         return r.status_code
 
+def who_am_i(ip):
+    print(ip)
+    w = whois.whois('77.106.112.231')
+    pprint(w)
+
+
 if __name__ == '__main__':
     try:
-        print(get_ip())
+        my_ip = get_ip()
+        who_am_i(my_ip)
     except Exception as error:
         print("Error:", error, "\nType:", type(error))
+
+
