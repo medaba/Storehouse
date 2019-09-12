bd1862b60d27c46d74ec6eb225bc2adbb1c6ec37
medaba@protonmail.com <medaba@protonmail.com>
2019-09-12 15:10:27 +0700

    IP whois

---
+++ myIP.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+
+import requests
+from whois import whois
+from pprint import pprint
+
+def get_ip():
+    """
+    Делает запрос к "api.ipify.org" и возвращает ваш текущий IP.
+    """
+    r = requests.get('https://api.ipify.org?format=json')
+    
+    if r.ok:
+        ip = r.json()["ip"]
+        return ip
+    else:
+        return r.status_code
+
+def whois_ip(ip):
+    """
+    Получение информации об IP либо домене.
+    """
+    w = whois(ip)
+    return {"ip/domain": ip, "whois": w}
+
+
+if __name__ == '__main__':
+    try:
+        print(get_ip())
+        pprint(whois_ip("https://medaba.pythonanywhere.com/"))
+    except Exception as error:
+        print("Error:", error, "\nType:", type(error))
+
+    
+
+
