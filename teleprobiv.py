f285924aa88f2a4b9e037537aa12e0ed37065f7c
medaba@protonmail.com <medaba@protonmail.com>
2019-09-13 13:55:00 +0700

    teleprobiv

---
+++ teleprobiv.py
@@ -0,0 +1,35 @@
+# Получение открытой информации о номере телефона.
+
+
+import requests
+import string
+from pprint import pprint
+
+
+def edit_string(proc_string):
+	"""
+	Принимает строку и удаляет из нее
+	все знаки препинания, символы и пробелы.
+	"""
+	symbols = string.punctuation + " "
+	for symbol in symbols:
+		proc_string = proc_string.replace(symbol, "")
+	return proc_string
+
+
+def phone_number(number):
+	"""
+	Принимает номер телефона (мобильный или стационар)
+	Возвращает json объект с информацией по этому номеру.
+	"""
+	number = str(number)
+	number = edit_string(number)  # Удаление из номера символов и пробелов.
+	r = requests.get(
+		f'https://htmlweb.ru/geo/api.php?json&telcod={number}'
+		)
+	return r.json()
+
+
+if __name__ == '__main__':
+	info = phone_number('+74993913590')
+	pprint(info)
