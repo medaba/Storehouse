a648059bacb4b1bfed72c95d09b1bed2147957ec
medaba <medaba@protonmail.com>
2020-06-18 15:14:53 +0700

    timer-decorator

--- function_timer.py
+++ function_timer.py
@@ -4,21 +4,21 @@ import time
 
 
 def func_timer(func):
-	"""Декоратор для вычисления времени отрабатывания принятой ф-и
-	"""
-	start_time = time.time()
-	func()
-	print(f"--- {time.time() - start_time} seconds ---")
+    """Декоратор для вычисления времени отрабатывания принятой ф-и
+    """
+    start_time = time.time()
+    func()
+    print(f"--- {time.time() - start_time} seconds ---")
 
 
 
 if __name__ == "__main__":
-	# Пример использования
+    # Пример использования
 
-	@func_timer
-	def some_func():
-		for i in range(20):
-			print(i)
+    @func_timer
+    def some_func():
+        for i in range(20):
+            print(i)
 
 
-	some_func()
+    some_func()
